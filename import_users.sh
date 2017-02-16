23a36bb996be9f163fa8576ff1c10056c24d6dca
Joshua Copeland <joshuarcopeland@gmail.com>
2017-02-16 11:47:37 -0800

    Update import_users.sh

--- import_users.sh
+++ import_users.sh
@@ -4,30 +4,49 @@
 # empty to not change sudo access, or give it the value '##ALL##' to have all
 # users be given sudo rights.
 SudoersGroup="${IAM_GRP_SUDOERS}"
-[[ -z "${SudoersGroup}" ]] || [[ "${SudoersGroup}" == "##ALL##" ]] || Sudoers=$(
+[[ -z "${SudoersGroup}" ]] || Sudoers=$(
   aws iam get-group --group-name "${SudoersGroup}" --query "Users[].[UserName]" --output text
 );
 
-aws iam list-users --query "Users[].[UserName]" --output text | while read User; do
-  SaveUserName="$User"
-  SaveUserName=${SaveUserName//"+"/".plus."}
-  SaveUserName=${SaveUserName//"="/".equal."}
-  SaveUserName=${SaveUserName//","/".comma."}
-  SaveUserName=${SaveUserName//"@"/".at."}
-  if ! grep "^$SaveUserName:" /etc/passwd > /dev/null; then
-    /usr/sbin/useradd --create-home --shell /bin/bash "$SaveUserName"
-  fi
+ShellAccessGroup="${IAM_GRP_SHELL_ACCESS}"
+[[ -z "${ShellAccessGroup}" ]] || ShellAccessUsers=$(
+  aws iam get-group --group-name "${ShellAccessGroup}" --query "Users[].[UserName]" --output text
+);
 
-  if [[ ! -z "${SudoersGroup}" ]]; then
-    # sudo will read each file in /etc/sudoers.d, skipping file names that end
-    # in ‘~’ or contain a ‘.’ character to avoid causing problems with package
-    # manager or editor temporary/backup files.
-    SaveUserFileName=$(echo "$SaveUserName" | tr "." " ")
-    SaveUserSudoFilePath="/etc/sudoers.d/$SaveUserFileName"
-    if [[ "${SudoersGroup}" == "##ALL##" ]] || echo "$Sudoers" | grep "^$User\$" > /dev/null; then
-      echo "$SaveUserName ALL=(ALL) NOPASSWD:ALL" > "$SaveUserSudoFilePath"
+aws iam list-users --query "Users[].[UserName]" --output text | while read User; do
+
+  if [[ ! -z "${ShellAccessGroup}" ]]; then
+    SaveUserName="$User"
+    SaveUserName=${SaveUserName//"+"/".plus."}
+    SaveUserName=${SaveUserName//"="/".equal."}
+    SaveUserName=${SaveUserName//","/".comma."}
+    SaveUserName=${SaveUserName//"@"/".at."}
+    if echo "$ShellAccessUsers" | grep "^$User\$" > /dev/null; then
+      # This is a user with shell access
+      if ! grep "^$SaveUserName:" /etc/passwd > /dev/null; then
+        /usr/sbin/useradd --create-home --shell /bin/bash "$SaveUserName"
+      fi
+
+      # Lets check if this user should have sudo access
+      if [[ ! -z "${SudoersGroup}" ]]; then
+        # sudo will read each file in /etc/sudoers.d, skipping file names that end
+        # in ‘~’ or contain a ‘.’ character to avoid causing problems with package
+        # manager or editor temporary/backup files.
+        SaveUserFileName=$(echo "$SaveUserName" | tr "." " ")
+        SaveUserSudoFilePath="/etc/sudoers.d/$SaveUserFileName"
+        if echo "$Sudoers" | grep "^$User\$" > /dev/null; then
+          echo "$SaveUserName ALL=(ALL) NOPASSWD:ALL" > "$SaveUserSudoFilePath"
+        else
+          [[ ! -f "$SaveUserSudoFilePath" ]] || rm "$SaveUserSudoFilePath"
+        fi
+      fi
     else
-      [[ ! -f "$SaveUserSudoFilePath" ]] || rm "$SaveUserSudoFilePath"
+      # This is a user without shell access
+      if grep "^$SaveUserName:" /etc/passwd > /dev/null; then
+        # If the user looks like it still exists, lets kill it
+        /usr/sbin/userdel -r "$SaveUserName"
+        /usr/sbin/groupdel "$SaveUserName"
+      fi
     fi
   fi
 done
