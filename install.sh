debe1e356fcf15883c60b170e12e0275b057dc32
Joshua Copeland <joshuarcopeland@gmail.com>
2017-02-16 11:15:26 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-tmpdir=`mktemp -d`
+# Install envplate for easy var replacement within files
+curl -sLo /usr/local/bin/ep https://github.com/kreuzwerker/envplate/releases/download/v0.0.8/ep-linux && chmod +x /usr/local/bin/ep
 
-cd $tmpdir
-
-# yum install -y git # if necessary
-# or download a tarball and decompress it instead
-git clone https://github.com/widdix/aws-ec2-ssh.git
-
-cd $tmpdir/aws-ec2-ssh
+# Replace the variables within scripts
+ep import_users.sh
 
 cp authorized_keys_command.sh /opt/authorized_keys_command.sh
 cp import_users.sh /opt/import_users.sh
-
-# To control which users are given sudo privileges, uncomment the line below
-# changing GROUPNAME to either the name of the IAM group for sudo users, or
-# to ##ALL## to give all users sudo access. If you leave it blank, no users will
-# be given sudo access.
-#sudo sed -i 's/SudoersGroup=""/SudoersGroup="GROUPNAME"/' /opt/import_users.sh
 
 sed -i 's:#AuthorizedKeysCommand none:AuthorizedKeysCommand /opt/authorized_keys_command.sh:g' /etc/ssh/sshd_config
 sed -i 's:#AuthorizedKeysCommandUser nobody:AuthorizedKeysCommandUser nobody:g' /etc/ssh/sshd_config
